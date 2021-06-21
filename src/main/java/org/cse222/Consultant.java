738497b36aecb99a5801a712f029774a6e05f636
Feyza Nur Akyol <fnuakyol@gmail.com>
2021-06-21 16:37:21 +0300

    Update Consultant.java

--- src/main/java/org/cse222/Consultant.java
+++ src/main/java/org/cse222/Consultant.java
@@ -1,12 +1,12 @@
-package org.cse222;
-
 import java.util.ArrayList;
 
-public class Consultant extends Person {
+public class Consultant extends Person{
 
     private int blockNumber;
 
     private int layerNumber;
+
+    private Database databaseRef;
 
 
     public Consultant(){
@@ -15,19 +15,71 @@ public class Consultant extends Person {
         this.layerNumber=0;
     }
 
-    public Consultant(String name, String surName, int age, char gender, Address address, int id, int blockNumber, int layerNumber){
+    public Consultant(Database database){
+        super();
+        this.blockNumber=0;
+        this.layerNumber=0;
+        databaseRef = database;
+    }
+
+    public Consultant(String name, String surName, int age, char gender,
+                      Address address, int id, int blockNumber, int layerNumber,
+                      Database database){
         super(name, surName, age, gender, address, id);
         this.blockNumber=blockNumber;
         this.layerNumber=layerNumber;
-    }
-    public void patientRegistration(ArrayList<Doctor> newDoctor){
-        
-        //coming patient informations will add into the database 
+        databaseRef = database;
     }
 
-    public void patientRegistration(Patient newPatient,ArrayList<Doctor> newDoctor){
-        
-        //coming patient informations will add into the database
+    public void ConsultantMenu(){
+        System.out.println("-------------------------");
+        System.out.println("-----Consultant Menu-----");
+        System.out.println("-------------------------");
+        System.out.println("--Patient Registration---");
+        System.out.println("-------------------------");
+
+    }
+
+    /**
+     * Adds the needed patient to needed doctor's
+     * queue.
+     * @param newPatient
+     * @param newDoctor
+     */
+    public void patientRegistration(Patient newPatient,Doctor newDoctor){
+
+        //I found needed doctor's place in database.
+        int neededDoctorIndex = databaseRef.getDoctors().indexOf(newDoctor);
+        databaseRef.getDoctors().get(neededDoctorIndex).getPatientList().add(newPatient);
+
+        System.out.println("Patient: "
+                + databaseRef.getDoctors().get(neededDoctorIndex).getPatientList().peek().getId()
+                +" "
+                + databaseRef.getDoctors().get(neededDoctorIndex).getPatientList().peek().getName()
+                +" "
+                + databaseRef.getDoctors().get(neededDoctorIndex).getPatientList().peek().getSurName()
+                +" Added to "
+                + databaseRef.getDoctors().get(neededDoctorIndex).getName()
+                +" "
+                + databaseRef.getDoctors().get(neededDoctorIndex).getSurName()
+        );
+    }
+
+    public static void main(String[] args) {
+        Database database = new Database();
+        database.getDoctors().add(new Doctor());
+        database.getDoctors().get(0).setName("Ali");
+
+        Consultant consultant = new Consultant(database);
+        consultant.ConsultantMenu();
+        consultant.patientRegistration(new Patient(),database.getDoctors().get(0));
+        consultant.patientRegistration(new Patient(),database.getDoctors().get(0));
+        consultant.patientRegistration(new Patient(),database.getDoctors().get(0));
+        consultant.patientRegistration(new Patient(),database.getDoctors().get(0));
+
+        System.out.println(database.getDoctors().get(0).getPatientList().size());
+
+
     }
 
 

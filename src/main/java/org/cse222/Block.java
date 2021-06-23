53e0f26540d8de916c02ba956dff733e6feee131
Feyza Nur Akyol <fnuakyol@gmail.com>
2021-06-24 02:52:57 +0300

    equals, toString methıds added

--- src/main/java/org/cse222/Block.java
+++ src/main/java/org/cse222/Block.java
@@ -35,4 +35,37 @@ public class Block {
     public ArrayList<Integer> getFloorList(){
         return floorIdList;
     }
+    
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Block block = (Block) o;
+        return blockNumber == block.blockNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(blockNumber);
+    }
+
+    @Override
+    public String toString() {
+        int k;
+        StringBuilder stringBuilder = new StringBuilder ();
+        for ( k = 0; k < 45; k++) stringBuilder.append ("-");
+        stringBuilder.append ("\n");
+        stringBuilder.append ("Block Name: "+blockNumber+"\n");
+        stringBuilder.append ("Floors in the Block: ");
+        for (Integer floor: floorIdList) {
+            stringBuilder.append (floor+",");
+        }
+        stringBuilder.deleteCharAt (stringBuilder.length ()-1);
+
+        stringBuilder.append ("\n");
+        for ( k = 0; k < 45; k++) stringBuilder.append ("-");
+        stringBuilder.append ("\n");
+        return stringBuilder.toString ();
+    }
 }

8327e23921ced17069eb9ea228a6068fc07da78a
Feyza Nur Akyol <fnuakyol@gmail.com>
2021-06-23 14:18:54 +0300

    database references added

--- src/main/java/org/cse222/ChiefPhysician.java
+++ src/main/java/org/cse222/ChiefPhysician.java
@@ -2,12 +2,19 @@ package org.cse222;
 
 public class ChiefPhysician extends Person {
 
+    private Database databaseRef;
+
     public ChiefPhysician(){
         super();
     }
 
-    public ChiefPhysician(String name, String surName, int age, char gender, Address address, int id){
+    public ChiefPhysician(String name, String surName, int age, char gender, Address address, int id, Database database){
         super(name, surName, age, gender, address, id);
+        databaseRef = database;
+    }
+
+    public ChiefPhysician(Database database){
+        databaseRef = database;
     }
 
     public boolean confirmOperation(Operation newOp){
--- src/main/java/org/cse222/Pharmacist.java
+++ src/main/java/org/cse222/Pharmacist.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 
 public class Pharmacist extends Person {
 
+    private Database databaseRef;
+
     public Pharmacist(){
         super();
     }
 
-    public Pharmacist(String name, String surName, int age, char gender, Address address, int id){
+    public Pharmacist(String name, String surName, int age, char gender, Address address, int id, Database database){
         super(name, surName, age, gender, address, id);
+        databaseRef = database;
     }
 
-
+    public Pharmacist(Database database){
+        databaseRef = database;
+    }
 
     public boolean seePrescription(ArrayList<String> recipeList){
 

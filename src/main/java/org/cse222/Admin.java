a5ce092d9e8b1023045a97df659d2d509ef5d7aa
Feyza Nur Akyol <fnuakyol@gmail.com>
2021-06-25 02:07:46 +0300

    Update Admin.java

--- src/main/java/org/cse222/Admin.java
+++ src/main/java/org/cse222/Admin.java
@@ -1,64 +1,83 @@
-package org.cse222;
+import java.util.Scanner;
 
 public class Admin extends Person {
-	public Admin(){
+    
+    Database databaseRef;
+    
+    public Admin(Database database){
         super();
+        databaseRef = database;
     }
 
-    public Admin(String name, String surName, int age, char gender, Address address, int id){
+    public Admin(String name, String surName, int age, char gender, 
+                 Address address, int id,Database database){
         super(name, surName, age, gender, address, id);
+        databaseRef = database;
+
     }
 
     public void assignNurseToFloor(Nurse newNurse, int blockNumber, int floorNumber){
-    	newNurse.setBlockNumber(blockNumber);
-    	newNurse.setLayerNumber(floorNumber);
-    	
+        newNurse.setBlockNumber(blockNumber);
+        newNurse.setLayerNumber(floorNumber);
+
     }
 
     public void assignConsultantToFloor(Consultant newConsultant, int blockNumber, int floorNumber){
-    	newConsultant.setBlockNumber(blockNumber);
-    	newConsultant.setLayerNumber(floorNumber);
+        newConsultant.setBlockNumber(blockNumber);
+        newConsultant.setLayerNumber(floorNumber);
 
     }
 
     public void assignDoctorToBlock(Doctor newDoctor, int blockNumber, int officeNumber, int floorNumber){
-    	newDoctor.setBlockNumber(blockNumber);
-    	newDoctor.setLayerNumber(floorNumber);
-    	newDoctor.setOfficeNumber(officeNumber);
+        newDoctor.setBlockNumber(blockNumber);
+        newDoctor.setLayerNumber(floorNumber);
+        newDoctor.setOfficeNumber(officeNumber);
     }
 
     public void assignTechnicalWorkerToBlock(TechnicianWorker newTechnicianWorker, int blockNumber){
-    		
-    	newTechnicianWorker.setblockNumber(blockNumber);
-    	
+        newTechnicianWorker.setBlockNumber(blockNumber);
+
     }
-	
-	private void menu(Personnel admin){
-		int k;
-		for ( k = 0; k < 45; k++) System.out.print("-");
-		System.out.print("\n"+"   ");
-		System.out.println("Welcome Admin! "+ getName()+" "+getSurname());
-		for ( k = 0; k < 45; k++) System.out.print("-");
-		System.out.print("\n");
-		System.out.println("[1] Assign Nurse to the floor");
-		for ( k = 0; k < 45; k++) System.out.print("-");  System.out.println();
-		System.out.println("[2] Assign Consultant to the floor");
-		for ( k = 0; k < 45; k++) System.out.print("-");  System.out.println();
-		System.out.println("[3] Assign Doctor To Block");
-		for ( k = 0; k < 45; k++) System.out.print("-");  System.out.println();
-		System.out.println("[5] Add all users in system");
-		for ( k = 0; k < 45; k++) System.out.print("-");  System.out.println();
-		System.out.println("[0] Exit.");
-		for ( k = 0; k < 45; k++) System.out.print("-");
-		System.out.print("\n");
-	}
-    
-    
-    
-    
-    
-    
-    
 
+    private void menu(){
+        String menuOpt = "notBack";
 
-}
+        while(!menuOpt.equals("back")){
+            System.out.println("-------------------------");
+            System.out.println("-----Admin Menu-----");
+            System.out.println("-------------------------");
+            System.out.println("--1) Add All Users ---");
+            System.out.println("-------------------------");
+            System.out.println("--2) Assign Nurse To The Floor---");
+            System.out.println("-------------------------");
+            System.out.println("--3) Assign Consultant To The Floor ---");
+            System.out.println("-------------------------");
+            System.out.println("--4) Assign Doctor To The Block ---");
+            System.out.println("-------------------------");
+            System.out.println("--5) Assign Technicial Worker To The Block ---");
+            System.out.println("-------------------------");
+            Scanner reader = new Scanner(System.in);
+
+            menuOpt = reader.nextLine();
+            
+            
+            if(menuOpt.equalsIgnoreCase("1")) ;
+            else if(menuOpt.equalsIgnoreCase("2")) {
+                System.out.println("Enter Nurse ID:");
+                int id = reader.nextInt();
+                System.out.println("Enter Block No:");
+                int block = reader.nextInt();
+                System.out.println("Enter Layer No:");
+                int layer  = reader.nextInt();
+                
+                databaseRef.findNurse(new Nurse(id,databaseRef)).setBlockNumber(block);
+                databaseRef.findNurse(new Nurse(id,databaseRef)).setLayerNumber(layer);
+                
+		//Print Result
+                System.out.println(databaseRef.findNurse(new Nurse(id,databaseRef)).toString());
+
+            }
+            else if(menuOpt.equalsIgnoreCase("back")) break;
+            else System.out.println("Unrecognized option. Try again.");
+        }
+    }

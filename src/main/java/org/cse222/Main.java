6e53c0aca1dcb33ad7bfbcc0270d160204d29618
Tunahan Karlibas <dvt.tnhn.krlbs@gmail.com>
2021-06-24 20:59:22 +0300

    fix gradlew wrapper

--- src/main/java/org/cse222/Main.java
+++ src/main/java/org/cse222/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //database created
-        Database database = new Database();
+        Database database= new Database();
 
         addUsers addUser = new addUsers();
         addUser.addConsultant(database);

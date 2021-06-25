039deaddc91aaf507cee81f67eefdcbfa68928e6
cagataycakir <cagatay1994@hotmail.com>
2021-06-25 22:26:12 +0300

    Update TechnicianWorker.java

--- src/main/java/org/cse222/TechnicianWorker.java
+++ src/main/java/org/cse222/TechnicianWorker.java
@@ -27,14 +27,24 @@ public class TechnicianWorker extends Person {
         databaseRef = database;
 
     }
+    
+    public TechnicianWorker(String name, String surName, int age, char gender, Address address
+            , int blockNumber, int layerNumber,Database database){
+        super(name, surName, age, gender, address, idNo);
+        this.blockNumber=blockNumber;
+        this.layerNumber=layerNumber;
+        databaseRef = database;
+        ++idNo;        
+        setJobType(JobType.TechnicianWorker);
+    }
 
     public void Menu(){
         Scanner reader = new Scanner(System.in);
             String menuOpt;
             System.out.print("Welcome back ");
             char gen = getGender();
-            if(gen=='f' || gen=='F') System.out.print("Mrs ");
-            else if(gen=='m' || gen=='M') System.out.print("Mr ");
+            if(gen=='g' || gen=='G') System.out.print("Mrs ");
+            else if(gen=='b' || gen=='B') System.out.print("Mr ");
             else System.out.print("Their ");
             System.out.println(getName());
             do{
@@ -58,16 +68,15 @@ public class TechnicianWorker extends Person {
                 System.out.println("Please enter patient name:");
                 patientn = reader.nextLine();
                 if(patientn.equalsIgnoreCase("back")) return;
-                mypat = databaseRef.getPatientByName(patientn);
+                mypat = databaseRef.getPatientByNo(Integer.parseInt(patientn));
                 if(mypat!=null) break;
                 System.out.println("This patient doesn't exist.");
             }while (true);
             ArrayList<String>anLines = new ArrayList<String>();
+            System.out.println("Fill analysis results below.");
+            System.out.println("Type \"save\" to update database.");
+            System.out.println("Type \"back\" to return without saving.");
             do{
-                //check patient
-                System.out.println("Fill analysis results below.");
-                System.out.println("Type \"save\" to update database.");
-                System.out.println("Type \"back\" to return without saving.");
                 line = reader.nextLine();
                 if(line.equalsIgnoreCase("save")) break;
                 if(line.equalsIgnoreCase("back")) return;

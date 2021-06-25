e600ef7ab6d843a102880639a33cc945e25cfdbf
Feyza Nur Akyol <fnuakyol@gmail.com>
2021-06-25 03:11:15 +0300

    Update addUsers.java

--- src/main/java/org/cse222/addUsers.java
+++ src/main/java/org/cse222/addUsers.java
@@ -4,27 +4,28 @@ import java.util.Hashtable;
 import java.util.TreeMap;
 
 public class addUsers {
+    
     public void addDoctors(Database databaseRef){
         Hashtable<Integer,Doctor> doctors = new Hashtable<>();
 
-        doctors.put(11111,new Doctor("Hayri","Tas",29,'b',new Address(),200,1,1,12,databaseRef));
-        doctors.put(11112,new Doctor("Ali","Yilmaz",56,'b',new Address(),201,2,1,12,databaseRef));
-        doctors.put(11113,new Doctor("Veli","Yilmaz",55,'b',new Address(),202,3,1,12,databaseRef));
-        doctors.put(11114,new Doctor("Handan","Ser",43,'b',new Address(),203,4,1,12,databaseRef));
-        doctors.put(11115,new Doctor("Tuba","Bak",42,'b',new Address(),204,5,1,12,databaseRef));
-        doctors.put(11116,new Doctor("Yasemin","Serin",44,'b',new Address(),205,6,2,12,databaseRef));
-        doctors.put(11117,new Doctor("Asiye","Kartal",46,'b',new Address(),206,7,2,12,databaseRef));
-        doctors.put(11118,new Doctor("Esra","Serim",36,'b',new Address(),207,8,2,12,databaseRef));
-        doctors.put(11119,new Doctor("Nevra","Merik",38,'b',new Address(),208,9,2,12,databaseRef));
-        doctors.put(11120,new Doctor("Esranur","Soysal",35,'b',new Address(),209,10,2,12,databaseRef));
-        doctors.put(11121,new Doctor("Fadime","Arıcan",33,'b',new Address(),210,12,3,12,databaseRef));
-        doctors.put(11122,new Doctor("Fatih","Ak",32,'b',new Address(),211,11,3,12,databaseRef));
-        doctors.put(11123,new Doctor("Murat","Sak",31,'b',new Address(),212,12,3,12,databaseRef));
-        doctors.put(11124,new Doctor("Erkan","Sengul",35,'b',new Address(),213,13,4,12,databaseRef));
-        doctors.put(11125,new Doctor("Görkem","Ayri",46,'b',new Address(),214,14,5,12,databaseRef));
-        doctors.put(11126,new Doctor("Yusuf","Anıl",34,'b',new Address(),215,15,5,12,databaseRef));
-        doctors.put(11127,new Doctor("Ahmet","Mert",29,'b',new Address(),216,16,5,12,databaseRef));
-        doctors.put(11128,new Doctor("Seda","Mert",29,'b',new Address(),217,17,5,12,databaseRef));
+        doctors.put(11111,new Doctor("Hayri","Tas",29,'b',new Address(),1,1,12,databaseRef));
+        doctors.put(11112,new Doctor("Ali","Yilmaz",56,'b',new Address(),2,1,12,databaseRef));
+        doctors.put(11113,new Doctor("Veli","Yilmaz",55,'b',new Address(),3,1,12,databaseRef));
+        doctors.put(11114,new Doctor("Handan","Ser",43,'b',new Address(),4,1,12,databaseRef));
+        doctors.put(11115,new Doctor("Tuba","Bak",42,'b',new Address(),5,1,12,databaseRef));
+        doctors.put(11116,new Doctor("Yasemin","Serin",44,'b',new Address(),6,2,12,databaseRef));
+        doctors.put(11117,new Doctor("Asiye","Kartal",46,'b',new Address(),7,2,12,databaseRef));
+        doctors.put(11118,new Doctor("Esra","Serim",36,'b',new Address(),8,2,12,databaseRef));
+        doctors.put(11119,new Doctor("Nevra","Merik",38,'b',new Address(),9,2,12,databaseRef));
+        doctors.put(11120,new Doctor("Esranur","Soysal",35,'b',new Address(),10,2,12,databaseRef));
+        doctors.put(11121,new Doctor("Fadime","Arıcan",33,'b',new Address(),12,3,12,databaseRef));
+        doctors.put(11122,new Doctor("Fatih","Ak",32,'b',new Address(),11,3,12,databaseRef));
+        doctors.put(11123,new Doctor("Murat","Sak",31,'b',new Address(),12,3,12,databaseRef));
+        doctors.put(11124,new Doctor("Erkan","Sengul",35,'b',new Address(),13,4,12,databaseRef));
+        doctors.put(11125,new Doctor("Görkem","Ayri",46,'b',new Address(),14,5,12,databaseRef));
+        doctors.put(11126,new Doctor("Yusuf","Anıl",34,'b',new Address(),15,5,12,databaseRef));
+        doctors.put(11127,new Doctor("Ahmet","Mert",29,'b',new Address(),16,5,12,databaseRef));
+        doctors.put(11128,new Doctor("Seda","Mert",29,'b',new Address(),17,5,12,databaseRef));
 
         databaseRef.setDoctors(doctors);
     }
@@ -32,49 +33,16 @@ public class addUsers {
     public void addConsultant(Database database){
 
         ArrayList<Consultant> consultants = new ArrayList<>();
-        consultants.add(new Consultant("Ayse","Alem",25,'g',new Address(),302,1,1,database));
-        consultants.add(new Consultant("Halime","Alem",25,'g',new Address(),302,2,2,database));
-        consultants.add(new Consultant("Servet","Ser",25,'g',new Address(),303,3,1,database));
-        consultants.add(new Consultant("Gul","Male",25,'g',new Address(),304,4,1,database));
+        consultants.add(new Consultant("Ayse","Alem",25,'g',new Address(),1,1,database));
+        consultants.add(new Consultant("Halime","Alem",25,'g',new Address(),2,2,database));
+        consultants.add(new Consultant("Servet","Ser",25,'g',new Address(),3,1,database));
+        consultants.add(new Consultant("Gul","Male",25,'g',new Address(),4,1,database));
 
         database.setConsultants(consultants);
     }
 
 
-    public void addMedicinePharmacy(Database database){
-        ArrayList<Medicine> medicines = new ArrayList<>();
-        medicines.add(new Medicine(1, "Parol", 0, null, 100, database));
-        medicines.add(new Medicine(2, "Calpol", 0, null, 10, database));
-        medicines.add(new Medicine(3, "Aspirin", 0, null, 5, database));
-        medicines.add(new Medicine(4, "Majezik", 0, null, 8, database));
-        medicines.add(new Medicine(5, "Dikloron", 0, null, 45, database));
-        medicines.add(new Medicine(6, "Akineton", 0, null, 40, database));
-        medicines.add(new Medicine(7, "Mestobran", 0, null, 3, database));
-        medicines.add(new Medicine(8, "Sotarmen", 0, null, 1, database));
-        medicines.add(new Medicine(9, "Metamil", 0, null, 6, database));
-        medicines.add(new Medicine(10, "Flagyl", 0, null, 7, database));
-
-        database.setMedicinePharmacy(medicines);
-    }
-
-    public void addMedicinePatient(Database database){
-        ArrayList<Medicine> medicines = new ArrayList<>();
-        medicines.add(new Medicine(1, "Parol", 2, "Sabah Akşam Aç Karnı", 0, database));
-        medicines.add(new Medicine(2, "Calpol", 3, "Günde 3 Öğün Tok Karnı", 0, database));
-        medicines.add(new Medicine(3, "Aspirin", 1, "Sabahları Aç Karnı", 0, database));
-        medicines.add(new Medicine(4, "Majezik", 1, "Sabah Akşam Tok Karnı", 0, database));
-        medicines.add(new Medicine(5, "Dikloron", 2, "Günde 3 Öğün Aç Karnı", 0, database));
-        medicines.add(new Medicine(6, "Akineton", 3, "Günde 1 Tok Karnı", 0, database));
-        medicines.add(new Medicine(7, "Mestobran", 2, "Günde 1 Aç Karnı", 0, database));
-        medicines.add(new Medicine(8, "Sotarmen", 1, "Sabahları Tok Karnı", 0, database));
-        medicines.add(new Medicine(9, "Metamil", 2, "Akşam Tok Karnı", 0, database));
-        medicines.add(new Medicine(10, "Flagyl", 1, "Akşam Aç Karnı", 0, database));
-
-        database.setMedicinePatient(medicines);
-    }
-    
-    
-public void addPatients(Database database){
+    public void addPatients(Database database){
         database.getAllPatients().add(new Patient("Ayse","Alem",25,'g',new Address(),false,1,false,false));
         database.getAllPatients().add(new Patient("Ali","Alem",25,'g',new Address(),false,1,false,false));
         database.getAllPatients().add(new Patient("Veli","Alem",25,'g',new Address(),false,1,false,false));
@@ -132,7 +100,7 @@ public void addPatients(Database database){
         database.getPatientRecords().add(new PatientRecord(database.getAllPatients().find(new Person(1125)), 11117));
 
     }
-    
+
     public void addNurse(Database database){
         database.getNurses().add(new Nurse("Amine","Alem",25,'g',new Address(),1,1,database));
         database.getNurses().add(new Nurse("Ayse","Alem",25,'g',new Address(),1,1,database));
@@ -146,9 +114,54 @@ public void addPatients(Database database){
         database.getNurses().add(new Nurse("Halim","Alem",27,'g',new Address(),2,1,database));
         database.getNurses().add(new Nurse("Selcuk","Alem",33,'g',new Address(),1,1,database));
 
+    }
+
+    public void addTech(Database database){
+        database.getTechnicianWorkers().add(new TechnicianWorker(database));
+        database.getTechnicianWorkers().add(new TechnicianWorker(database));
+        database.getTechnicianWorkers().add(new TechnicianWorker(database));
+        database.getTechnicianWorkers().add(new TechnicianWorker(database));
+        database.getTechnicianWorkers().add(new TechnicianWorker(database));
+        database.getTechnicianWorkers().add(new TechnicianWorker(database));
+        database.getTechnicianWorkers().add(new TechnicianWorker(database));
 
 
     }
+   
+
+    public void addMedicinePharmacy(Database database){
+        ArrayList<Medicine> medicines = new ArrayList<>();
+        medicines.add(new Medicine(1, "Parol", 0, null, 100, database));
+        medicines.add(new Medicine(2, "Calpol", 0, null, 10, database));
+        medicines.add(new Medicine(3, "Aspirin", 0, null, 5, database));
+        medicines.add(new Medicine(4, "Majezik", 0, null, 8, database));
+        medicines.add(new Medicine(5, "Dikloron", 0, null, 45, database));
+        medicines.add(new Medicine(6, "Akineton", 0, null, 40, database));
+        medicines.add(new Medicine(7, "Mestobran", 0, null, 3, database));
+        medicines.add(new Medicine(8, "Sotarmen", 0, null, 1, database));
+        medicines.add(new Medicine(9, "Metamil", 0, null, 6, database));
+        medicines.add(new Medicine(10, "Flagyl", 0, null, 7, database));
+
+        database.setMedicinePharmacy(medicines);
+    }
+
+    public void addMedicinePatient(Database database){
+        ArrayList<Medicine> medicines = new ArrayList<>();
+        medicines.add(new Medicine(1, "Parol", 2, "Sabah Akşam Aç Karnı", 0, database));
+        medicines.add(new Medicine(2, "Calpol", 3, "Günde 3 Öğün Tok Karnı", 0, database));
+        medicines.add(new Medicine(3, "Aspirin", 1, "Sabahları Aç Karnı", 0, database));
+        medicines.add(new Medicine(4, "Majezik", 1, "Sabah Akşam Tok Karnı", 0, database));
+        medicines.add(new Medicine(5, "Dikloron", 2, "Günde 3 Öğün Aç Karnı", 0, database));
+        medicines.add(new Medicine(6, "Akineton", 3, "Günde 1 Tok Karnı", 0, database));
+        medicines.add(new Medicine(7, "Mestobran", 2, "Günde 1 Aç Karnı", 0, database));
+        medicines.add(new Medicine(8, "Sotarmen", 1, "Sabahları Tok Karnı", 0, database));
+        medicines.add(new Medicine(9, "Metamil", 2, "Akşam Tok Karnı", 0, database));
+        medicines.add(new Medicine(10, "Flagyl", 1, "Akşam Aç Karnı", 0, database));
+
+        database.setMedicinePatient(medicines);
+    }
+    
+    
 
     public void addChiefPhysician(Database database){
         new ChiefPhysician("Merve", "Gürler", 55, 'g', new Address(), 111, database);
